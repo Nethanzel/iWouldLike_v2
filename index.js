d0dcf606d845cacf3db21c1db804fcb700b7a8c5
Nethanzel <natanaelabaad@gmail.com>
2021-10-18 22:39:52 -0400

    -

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 const sFavicon = require("serve-favicon");
 
 require("./src/data/dataAcces");
-//require("./src/mailer/start");
+require("./src/mailer/start");
 require("./src/mailer/summary");
 
 const validation = require("./CrossCuttingConcerns/Auth/clientValidation.js");
